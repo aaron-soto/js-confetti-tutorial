3b64062809e1006d2879d7e7d0793995955fbed2
Aaron Soto <aaron.m.soto1@gmail.com>
2022-11-03 09:07:43 -0700

    setup animation loop

--- index.js
+++ index.js
@@ -4,5 +4,13 @@ const c = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-c.fillStyle = 'black';
-c.fillRect(100, 200, 50, 150);
+const animate = () => {
+	requestAnimationFrame(animate);
+	// Erase everything on canvas so next frame can be drawn
+	c.clearRect(0, 0, canvas.width, canvas.height);
+
+	c.fillStyle = 'black';
+	c.fillRect(100, 200, 50, 150);
+};
+
+animate();
